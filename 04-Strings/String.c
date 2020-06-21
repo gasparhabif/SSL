fff7917e8534245f3e5b540e156be885eb50a74e
Gaspar Habif <gasparhabif@gmail.com>
2020-06-21 12:36:19 -0300

    Developed Power and Substring and corrected .h types

--- 04-Strings/String.c
+++ 04-Strings/String.c
@@ -5,23 +5,48 @@
 
 #include "String.h"
 
-size_t GetLength(const char *characterString) {
-    size_t index = 0;
-    while (characterString[index] != '\0')
-        index++;
+unsigned int GetLength(const char *characterString)
+{
+    unsigned int i = 0;
+    while (characterString[i] != '\0')
+        i++;
 
-    return index;
+    return i;
 }
 
-bool IsEmpty(const char *characterString) {
+bool IsEmpty(const char *characterString)
+{
     return GetLength(characterString) == 0;
 }
 
-char *Power(const char *characterString, unsigned int power) {
-    return "";
+char *Power(const char *characterString, unsigned int power)
+{
+    int length = GetLength(characterString);
+    int returnLength = length * power;
+
+    char *returnString = (char *)malloc((returnLength + 1) * sizeof(char));
+
+    for (unsigned int i = 0; i < returnLength; ++i)
+    {
+        returnString[i] = characterString[i % length];
+    }
+
+    returnString[returnLength] = '\0';
+
+    return returnString;
 }
 
-char *Substring(const char *characterString, unsigned int start, unsigned int length);
+char *Substring(const char *characterString, unsigned int start, unsigned int end)
 {
-    return "";
+    unsigned int returnLength = start - end;
+    char *returnString = (char *)malloc((returnLength + 1) * sizeof(char));
+
+    for (unsigned int i = 0; i < end - start; ++i)
+    {
+        returnString[i] = characterString[start + i];
+    }
+
+    returnString[returnLength] = '\0';
+
+    return returnString;
 }
--- 04-Strings/String.h
+++ 04-Strings/String.h
@@ -5,38 +5,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 
 // GetLength
-// - Precondición y Argumentos: Consta de un unico argumento que sera una cadena que se espera, 
+// - Precondición y Argumentos: Consta de un unico argumento que sera una cadena que se espera,
 //   finalice en caracter nulo ('\0').
 // - Postcondición y Resultados: Retorna un entero positivo, incluyendo el cero, que representa la cantidad de
 //   caracteres que componen la cadena de la precondición.
-size_t GetLength(const char * characterString);
-
+unsigned int GetLength(const char *characterString);
 
 // IsEmpty
-// - Precondición y Argumentos: Consta de un unico argumento que sera una cadena que se espera, 
+// - Precondición y Argumentos: Consta de un unico argumento que sera una cadena que se espera,
 //   finalice en caracter nulo ('\0').
 // - Postcondición y Resultados: Retorna un valor booleano, donde True representa que la cadena está vacía
 //   y False que la cadena contiene algun caracter.
-bool IsEmpty(const char * characterString);
-
+bool IsEmpty(const char *characterString);
 
 // Power
-// - Precondición y Argumentos: Consta de dos argumentos, por una parte characterString que sera 
+// - Precondición y Argumentos: Consta de dos argumentos, por una parte characterString que sera
 //   una cadena que se espera, finalice en caracter nulo ('\0'). Y por otra parte power que
 //   representa la cantidad de veces que se repetira la cadena
-// - Postcondición y Resultados: Retorna un puntero a una cadena que contiene 
+// - Postcondición y Resultados: Retorna un puntero a una cadena que contiene
 //   el resultado de potenciar la cadena.
-char * Power(const char * characterString, unsigned int power);
-
+char *Power(const char *characterString, unsigned int power);
 
 // Substring
-// - Precondición y Argumentos: Consta de tres argumentos; 
+// - Precondición y Argumentos: Consta de tres argumentos;
 //         1. characterString que sera una cadena que se espera, finalice en caracter nulo ('\0').
 //         2. start que representa el indice inicial desde el que se buscar recortar
-//         3. length que representa la cantidad de caracteres que quieren separarce
-// - Postcondición y Resultados: Retorna un puntero a una cadena que contiene 
+//         3. end que representa el indice final (exluyente) hasta el que se busca recortar
+// - Postcondición y Resultados: Retorna un puntero a una cadena que contiene
 //   el resultado de recortar la cadena con el indice y la longitud indicadas.
-char * Substring(const char * characterString, unsigned int start, unsigned int length);
+char *Substring(const char *characterString, unsigned int start, unsigned int end);
