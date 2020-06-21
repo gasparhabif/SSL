d4ef4b76b1ce0d108e1db65dd46936742bc7232f
Gaspar Habif <gasparhabif@gmail.com>
2020-06-21 13:55:09 -0300

    Moved stuff and developed tests

--- 04-Strings/StringTest.c
+++ 04-Strings/StringTest.c
@@ -2,10 +2,11 @@
 #include "String.h"
 
 // Test GetLength()
-void testGetLength(){
+void testGetLength()
+{
     // Test cadena estática con constante de longitud 8
     char wordWithLength[8 + 1] = "Sintaxis";
-    assert(GetLength(wordWithLength) == 5); 
+    assert(GetLength(wordWithLength) == 5);
 
     // Test cadena estática con constante de longitud 0
     char wordWithNoLength[8 + 1] = "";
@@ -13,19 +14,20 @@ void testGetLength(){
 
     // Test cadena constante con longitud 3
     size_t length3 = GetLength("SSL");
-    assert(length3 == 3); 
+    assert(length3 == 3);
 
     // Test cadena constante con longitud 0
     size_t length0 = GetLength("");
-    assert(length0 == 0); 
+    assert(length0 == 0);
 }
 
 // Test IsEmpty()
-void testIsEmpty(){
+void testIsEmpty()
+{
 
     // Test cadena estática con constante no vacía
     char notEmptyWord[8 + 1] = "Sintaxis";
-    assert(!IsEmpty(notEmptyWord)); 
+    assert(!IsEmpty(notEmptyWord));
 
     // Test cadena estática con constante vacía
     char emptyWord[8 + 1] = "";
@@ -33,9 +35,34 @@ void testIsEmpty(){
 
     // Test cadena constante no vacía
     bool notEmpty = IsEmpty("SSL");
-    assert(!notEmpty); 
+    assert(!notEmpty);
 
     // Test cadena constante vacía
     bool empty = IsEmpty("");
-    assert(empty); 
+    assert(empty);
+}
+
+// Test Power
+void testPower()
+{
+    // Test cadena estática constante no vacía con potenciador de 3
+    int power = 3;
+    char initialWord[3 + 1] = "SSL";
+    char expectedWord[9 + 1] = "SSLSSLSSL";
+
+    assert(Power(initialWord, power) == expectedWord);
+
+    // Test cadena constante con longitud esperada de 9
+    char *resultWord = Power("SSL", power);
+    assert(GetLength(resultWord) == 9);
+}
+
+// Test Substring
+void testPower()
+{
+    // Test cadena estática constante
+    char initialWord[37 + 1] = "Sintaxis y Semantica de los Lenguajes";
+    char expectedWord[8 + 1] = "Sintaxis";
+
+    assert(Substring(initialWord, 0, 9) == expectedWord);
 }
---
+++ 05-Interfaces/Conversion.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define STEP 20 /* step size */
+#define MIN 0   /* minimum limit of table */
+#define MAX 300 /* maximum limit of table */
+
+int main(void)
+{
+}
