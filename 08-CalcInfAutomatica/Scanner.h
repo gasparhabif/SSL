0cd63b31137ce9a2d41767de8f4c6f05a529363c
Gaspar Habif <gasparhabif@gmail.com>
2021-03-04 19:52:34 -0300

    Removed unnecessary defines

--- 08-CalcInfAutomatica/Scanner.h
+++ 08-CalcInfAutomatica/Scanner.h
@@ -1,18 +1,6 @@
 #include "Constants.h"
 #include "Buffer.h"
 
-// Possible Constants
-#define POSSIBLE_IDS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#define POSSIBLE_NUM "0123456789"
-
-// Possible Operators
-#define ADDITION_OP "+"
-#define PRODUCT_OP "*"
-#define ASSIGNATION_OP "="
-
-#define PARENTHESIS_OP "("
-#define PARENTHESIS_CL ")"
-
 enum Tokens
 {
     INITIAL = 0,
