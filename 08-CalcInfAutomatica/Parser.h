2b614f92e4799b93c8fa4aa0cca840386607a30b
Gaspar Habif <gasparhabif@gmail.com>
2021-03-10 18:58:35 -0300

    Refactor

--- 08-CalcInfAutomatica/Parser.h
+++ 08-CalcInfAutomatica/Parser.h
@@ -7,9 +7,10 @@ static bool openedAssignation = false;
 
 void RunProgram();
 static void Program(Token ct, Token lT, int lO);
-void CheckToken(Token cT, Token lT);
+static void CheckToken(Token cT, Token lT);
 static bool IsTokenOperator(Token t);
 static bool IsTokenConstant(Token t);
 static int EvaluateExpresion(Token t, int i);
+static void ResolveExpresion(Token t, int i);
 static void PrintResult();
 static void CleanGlobalVariables();
